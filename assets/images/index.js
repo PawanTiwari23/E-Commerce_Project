9cac88f637353bb908c541045f06a614c1841538
PawanTiwari23 <166102315+PawanTiwari23@users.noreply.github.com>
2025-01-29 04:40:18 -0800

    Add files via upload

---
+++ assets/images/index.js
@@ -0,0 +1,55 @@
+import emptyCart from '../images/emptyCart.png';
+import bannerImgOne from './banner/bannerImgOne.webp';
+import bannerImgThree from './banner/bannerImgOne.webp';
+import bannerImgTwo from './banner/bannerImgTwo.webp';
+import logoLight from './logoLight.png';
+import logo from './orebiLogo.png';
+// ============== Products End here ======================
+import paymentCard from './payment.png';
+import bestSellerFour from './products/bestSeller/bestSellerFour.webp';
+// Best Sellers
+import bestSellerOne from './products/bestSeller/bestSellerOne.webp';
+import bestSellerThree from './products/bestSeller/bestSellerThree.webp';
+import bestSellerTwo from './products/bestSeller/bestSellerTwo.webp';
+import newArrFour from './products/newArrival/newArrFour.webp';
+// ============== Products Start here ====================
+// New Arrivals
+import newArrOne from './products/newArrival/newArrOne.webp';
+import newArrThree from './products/newArrival/newArrThree.webp';
+import newArrTwo from './products/newArrival/newArrTwo.webp';
+// Year Product
+import productOfTheYear from './products/productOfTheYear.webp';
+import spfFour from './products/specialOffer/spfFour.webp';
+// Special Offers
+import spfOne from './products/specialOffer/spfOne.webp';
+import spfThree from './products/specialOffer/spfThree.webp';
+import spfTwo from './products/specialOffer/spfTwo.webp';
+import saleImgOne from './sale/saleImgOne.webp';
+import saleImgThree from './sale/saleImgThree.webp';
+import saleImgTwo from './sale/saleImgTwo.webp';
+
+export {
+  bannerImgOne,
+  bannerImgThree,
+  bannerImgTwo,
+  bestSellerFour,
+  bestSellerOne,
+  bestSellerThree,
+  bestSellerTwo,
+  emptyCart,
+  logo,
+  logoLight,
+  newArrFour,
+  newArrOne,
+  newArrThree,
+  newArrTwo,
+  paymentCard,
+  productOfTheYear,
+  saleImgOne,
+  saleImgThree,
+  saleImgTwo,
+  spfFour,
+  spfOne,
+  spfThree,
+  spfTwo,
+};
